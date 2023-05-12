a26ca61b66cca3bb1ff338c4b0f9ce6a45027761
shraaavss <shravs2707@gmail.com>
2023-05-12 11:34:47 +0530

    states for name working

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function App(){
+
+    const [name, setName] = useState();
+
+    function handleSub(){
+        const inp = document.getElementById("username");
+        setName(inp.value);
+        // console.log(name);
+    }
+    
     
     return(
-            <div className="papaDiv">
-                <h1 className="question"> What's Your Name? </h1>
-                <form className="takeInput">
-                    <input autoFocus type="text" id="username"/>
-                </form>
-            </div>
+        <div className="papaDiv">
+            <h1 className="question"> What's Your Name? </h1>
+            <form className="takeInput" onSubmit={handleSub}>
+                <input autoFocus type="text" id="username"/>
+            </form>
+            {/* {console.log(name)} STATES FOR NAME IS WORKING*/}
+        </div>
+
     )
 }
 
