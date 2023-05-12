6c0fbb0046391db1949ea4506cdf70d382fa3095
shraaavss <shravs2707@gmail.com>
2023-05-12 07:26:58 +0530

    started working - 12/05/23

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App(){
+    
+    return(
+            <div className="papaDiv">
+                <h1 className="randText">Lorem ipsum dolor sit amet</h1>
+            </div>
+    )
+}
