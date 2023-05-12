81566107f5e0ca6492211246e64b79311347c22c
shraaavss <shravs2707@gmail.com>
2023-05-12 08:25:01 +0530

    Achieved the box

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ export default function App(){
     
     return(
             <div className="papaDiv">
-                <h1 className="randText">Lorem ipsum dolor sit amet</h1>
+                <h1 className="question"> What's Your Name? </h1>
+                <form className="takeInput">
+                    <input autoFocus type="text" id="username"/>
+                </form>
             </div>
     )
 }
+
+// condition ? exprIfTrue : exprIfFalse ==> Ternary Operator
