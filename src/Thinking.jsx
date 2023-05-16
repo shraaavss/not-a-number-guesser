5370bd8f0cf373d439253802348c9b6769147d81
shraaavss <shravs2707@gmail.com>
2023-05-16 10:50:03 +0530

    warnings dealt

--- src/Thinking.jsx
+++ src/Thinking.jsx
@@ -1,20 +1,18 @@
 import React, {useState, useEffect} from "react";
-// import App from "./App";
-import UserNum from "./UserNum";
-
 
 export default function ThinkDoing(props){
-
-    var brain = ["Connecting to the database...", "Collecting required data...", "Looking into the stars...", "Mining the Earth...", "Catching the brain waves...",
-                "Contacting your thoughts...", "Checking the libraries...", "Flipping the pages...", "Calculating probabilities...", "Checking the storage...", "Hacking your phone..."];
-    
-    const randomElement = brain[Math.floor(Math.random() * brain.length)];
-    const randomElement2 = brain[Math.floor(Math.random() * brain.length)];
-    const randomElement3 = brain[Math.floor(Math.random() * brain.length)];
 
     const [BrainContent, SetBrainContent] = useState("Hmm, let me see what I can do...");
 
     useEffect(()=> {
+        
+        var brain = ["Connecting to the database...", "Collecting required data...", "Looking into the stars...", "Mining the Earth...", "Catching the brain waves...",
+                    "Contacting your thoughts...", "Checking the libraries...", "Flipping the pages...", "Calculating probabilities...", "Checking the storage...", "Hacking your phone..."];
+    
+        const randomElement = brain[Math.floor(Math.random() * brain.length)];
+        const randomElement2 = brain[Math.floor(Math.random() * brain.length)];
+        const randomElement3 = brain[Math.floor(Math.random() * brain.length)];
+
         setTimeout(() => {
             SetBrainContent(randomElement);
         }, 2000);
@@ -32,7 +30,7 @@ export default function ThinkDoing(props){
             SetBrainContent(<span>The number you're thinking of is: {props.numba}</span>);     
         }, 8000);
 
-    }, []);
+    }, [props.numba]);
 
     return(
         <div className="container">
